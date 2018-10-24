655f7aeaf42788df9267a702e513bd94f65ada13
himanshush200599 <himanshush214@gmail.com>
2018-10-24 07:18:19 -0400

    implement add notes functionality

--- notes.js
+++ notes.js
@@ -1,7 +1,40 @@
 console.log("notes.js is starting ");
+const fs  = require("fs");
+var fetchNotes = () =>{
+  try {
+    var notesString = fs.readFileSync('notes.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return []
+  }
+}
+var saveNotes = (notes) =>{
+  fs.writeFileSync("notes.json",JSON.stringify(notes));
 
+}
 var addNotes = (title,body) =>{
-  console.log("Title is ",title,": Body is ",body);
+  var  notes  = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+  try {
+    var notesString = fs.readFileSync('notes.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
+  }
+
+var duplicatesNotes = notes.filter((note)=> note.title === title);
+if(duplicatesNotes.length===0){
+  notes.push(note);
+  saveNotes(notes);
+  console.log(`Title is : ${note.title}`);
+  console.log(`Body is :  ${note.body}`);
+}
+else{
+  console.log("Notes of same title is already in the database");
+}
+
 };
 var getNotes = () =>{
   console.log("getting all notes ");
